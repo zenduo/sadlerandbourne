cc0a8ad489c3b20bcb04dc934338afa033769ac1
zenduo <alan@zenduo.co.uk>
2013-02-12 21:01:36 +0000

    Created contact page and form layout

---
+++ contact.php
@@ -0,0 +1,78 @@
+<?php include('includes/header.php');?>
+  <!-- END MAIN CONTAINER -->
+  <div class="container">
+    <!-- ABOUT TOP -->
+    <div class="row">
+      
+      
+
+      <!-- MAP -->
+      <div class="span6 pull-right">
+        <img src="img/plumber-liskeard-map.png" alt="">
+      </div><!-- END MAP -->
+
+      <!-- ABOUT TEXT -->
+      <div class="span6 pull-left">
+        <h1><em>Contact</em> us</h1>
+        <p><strong>If you’re interested in any of our services or want to know more, give us a call, drop us an email or use our contact form below.</strong></p>
+        <p>We’d love to hear from you to discuss your requirements and how Sadler &amp; Bourne can help you!</p>
+      </div><!-- ABOUT TEXT END -->
+
+    </div><!-- END ABOUT TOP -->
+
+</div>
+
+   <div class="container">
+
+    <div class="row">
+      <div class="span12"><h1><em>Send us a</em> message</h5></div>
+    </div>
+  </div>
+
+  <div class="container message">
+    <div class="row">
+      <form>
+        <fieldset>
+          <div class="span5">
+            <label>Name</label>
+            <input type="text" placeholder="John Smith">
+
+            <label>Email address</label>
+            <input type="text" placeholder="you@youremail.com">
+
+            <label>Telephone number</label>
+            <input type="text" placeholder="Number and Area code">
+          </div>
+          <div class="span7">
+            <label>Message</label>
+            <textarea rows="3" placeholder="Type something…"></textarea>
+            <br/>
+            <button type="submit" class="btn">Send message</button>
+          </div>
+      </fieldset>
+    </form>
+    </div>
+
+  </div>
+    
+
+
+
+   <div class="container aboutTwitter">
+
+    <div class="row">
+      <div class="span5">
+        <h4>follow us on twitter: <a href="http://twitter.com/sadlerandbourne/" target="_blank">@SadlerAndBourne</a></h4>
+      </div>
+      <div class="span7"><p>“This is one hundred and forty characters and its pretty long if you ask me. I'm getting bored here so thank god its almost over... done!”</p></div>
+    </div>
+
+  </div>
+
+
+  
+  
+
+  <div class="container">       
+
+<?php include('includes/footer.php');?>
